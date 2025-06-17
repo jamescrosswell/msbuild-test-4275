3cad0cd37345ea3d7f6efbc04a5c8b7a1b3e8f79
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-06-17 13:53:14 +1200

    Initial commit

---
+++ TestConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿using TestLibrary;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Framework Information:");
+Console.WriteLine(TestClass.GetFrameworkInfo());
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace TestLibrary;
+
+public class TestClass
+{
+    public static string GetFrameworkInfo()
+    {
+        var info = new System.Text.StringBuilder();
+        info.AppendLine($"Running on: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
+        
+#if NET8_OR_GREATER
+        info.AppendLine("NET8_OR_GREATER is defined");
+#endif
+
+#if NET9_OR_GREATER
+        info.AppendLine("NET9_OR_GREATER is defined");
+#endif
+
+        return info.ToString();
+    }
+}
